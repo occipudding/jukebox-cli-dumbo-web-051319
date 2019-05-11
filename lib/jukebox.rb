bd09735fcddd4dfd616f65cd2be3fc89af398d47
Joshua Suskin <jsuskin85@gmail.com>
2019-05-11 03:25:23 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   choice = STDIN.gets.chomp
   if choice =~ /\d+/
-    puts "Playing #{songs[choice.to_i - 1]}"
+    puts "Playing #{songs[Integer(choice) - 1]}"
   elsif songs.include?(choice)
     puts "Playing #{songs[songs.index(choice)]}"
   else
